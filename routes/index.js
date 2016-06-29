7a7c3c5db7d1176d48e9cfc942f0312f6f2c5d24
lwj <1198275198@qq.com>
2016-06-29 16:44:48 +0800

    test use connect-flash

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/a', function(req, res, next){
+    req.flash('m', 'his');
+    res.redirect('/');
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'KnowTrans-To Know And To Transmit' });
+    
+    res.render('index', { title: 'KnowTrans-To Know And To Transmit' , m:req.flash('m')});
 });
 
 module.exports = router;
